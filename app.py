382f703b6a74ca491cb66df85019143308ee74e0
ANUKULI SALMAN RAJU <anukulisalmanraju700@gmail.com>
2026-08-09 19:00:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1171,9 +1171,4 @@ def home():
 # =========================================
 
 if __name__ == "__main__":
-
-    app.run(
-        debug=True,
-        host="127.0.0.1",
-        port=5000
-    )
+    app.run(port=5001)
